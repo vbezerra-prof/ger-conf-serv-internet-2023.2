e14036d4f9f8dcfdd58cf1d7410c2d4b0d031f22
Vagner Bezerra <116462099+vbezerra-prof@users.noreply.github.com>
2023-08-25 22:37:04 -0400

    Atualização Hello World Docker

--- 01-introducao-docker/script.sh
+++ 01-introducao-docker/script.sh
@@ -1,7 +1,33 @@
-# Hello World
-docker run hello-world
+###### COMANDOS BASICOS ######
 
-# Container Nginx
-docker run --name nginx1 -p 80:80 -d nginx:1.25-alpine
-docker run --name nginx2 -p 81:80 -d nginx:1.25-alpine
-docker run --name nginx3 -p 82:80 -d nginx:1.25-alpine
+# Criar e executar um container Hello World
+docker run hello-world # Cria um container de forma persistente
+docker run --rm hello-world # Cria um container temporário
+
+# Listar os containers
+docker container list # Comando que lista todos os containers em execução
+docker ps -a # Comando que lista todos os containers, mesmo parados
+
+# Listar imagens offline (cache do docker hub)
+docker image list
+
+# Comandos de gerenciamento de estado do container
+docker start NOME_DO_CONTAINER ou ID_DO_CONTAINER # Iniciar
+docker stop NOME_DO_CONTAINER ou ID_DO_CONTAINER # Parar
+docker restart NOME_DO_CONTAINER ou ID_DO_CONTAINER # Reiniciar
+docker rm NOME_DO_CONTAINER ou ID_DO_CONTAINER # Remover
+
+# Comando em lote para gerenciar containers a partir dos IDs (Dica: usar em ambiente de testes)
+docker start $(docker ps -a -q) # Iniciar todos
+docker stop $(docker ps -a -q) # Parar todos
+docker restart $(docker ps -a -q) # Reiniciar todos
+docker rm $(docker ps -a -q) # Remover todos
+
+# Zerar o docker
+docker system prune --all -f
+
+
+###### CONTAINER COM EXECUÇÃO CONSTANTE ######
+docker run --name nginx1 -p 80:80 -d --rm nginx:1.25-alpine
+docker run --name nginx2 -p 81:80 -d --rm nginx:1.25-alpine
+docker run --name nginx3 -p 82:80 -d --rm nginx:1.25-alpine
