ae11a62b5cc1fa75687f108b32b7ef80f95e5cea
Vagner Bezerra <116462099+vbezerra-prof@users.noreply.github.com>
2023-09-15 23:03:11 -0400

    Aula node-app

--- 02-imagem/03-node-app/script.sh
+++ 02-imagem/03-node-app/script.sh
@@ -1,10 +1,12 @@
 # -> Compilar a imagem
-
+docker build -t node-app .
 
 # -> Listar Imagens
-
+docker image list
 
 # -> # Criar container a partir da imagem criada
-
+docker run --name node-app-c1 --rm -p 8080:3000 -d node-app
+docker run --name node-app-c2 --rm -p 8081:3000 -d node-app
 
 # Listar Containers
+docker ps
