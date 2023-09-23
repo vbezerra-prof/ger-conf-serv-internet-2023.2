d023756439b665fe21cb133ee485013044200f8f
Vagner Bezerra <116462099+vbezerra-prof@users.noreply.github.com>
2023-09-22 22:49:19 -0400

    Scripts aula nginx volume

--- 03-volume/01-nginx-html/script.sh
+++ 03-volume/01-nginx-html/script.sh
@@ -1,11 +1,15 @@
 # -> Compilar a nova imagem a partir do arquivo dockerfile sem volume
-
+docker image build -f dockerfile-html -t nginx-html .
+docker image ls
 
 # -> Criar um novo container a partir da imagem criada em modo detached
-
-
+docker run --name nginx-c1 --rm -d -p 80:80 nginx-html
+docker run --name nginx-c2 --rm -d -p 81:80 nginx-html
+docker ps -a
 
 # -> Compilar a nova imagem a partir do arquivo dockerfile com volume
-
+docker image build -f dockerfile-html-volume -t nginx-html-volume .
 
 # -> Criar um novo container a partir da imagem criada em modo detached
+docker run --name nginx-vol-c1 --rm -d -p 90:80 -v $(pwd)/src:/usr/share/nginx/html nginx-html-volume
+docker run --name nginx-vol-c2 --rm -d -p 91:80 -v $(pwd)/src:/usr/share/nginx/html nginx-html-volume
