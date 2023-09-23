aee6269f2994f9731cbee396e8a1ed411e4168ba
Vagner Bezerra <116462099+vbezerra-prof@users.noreply.github.com>
2023-09-22 21:32:22 -0400

    Aula docker volume

---
+++ 03-volume/01-nginx-html/script.sh
@@ -0,0 +1,11 @@
+# -> Compilar a nova imagem a partir do arquivo dockerfile sem volume
+
+
+# -> Criar um novo container a partir da imagem criada em modo detached
+
+
+
+# -> Compilar a nova imagem a partir do arquivo dockerfile com volume
+
+
+# -> Criar um novo container a partir da imagem criada em modo detached
