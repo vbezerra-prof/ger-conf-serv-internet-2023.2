b062995849e9010a2d26f4a21e9c505571c1084d
Vagner Bezerra <116462099+vbezerra-prof@users.noreply.github.com>
2023-11-24 19:18:21 -0400

    Exemplo Docker Volume MariaDB

---
+++ 03-volume/02-mariadb/script.sh
@@ -0,0 +1,17 @@
+# -> Criar um volume para armazenar dados do mariadb
+docker volume create mariadb-volume
+
+# -> Criar um novo container do mariadb
+docker run --rm -d --name mariadb \
+-e MARIADB_ROOT_PASSWORD='123456' \
+-v mariadb-volume:/var/lib/mysql \
+-p 3306:3306 \
+mariadb:11
+
+# Com a extensão do Vscode Database Client, crie um nova conexão com o banco de dados
+# Nome conexão: mariadb-docker
+# Host: 127.0.0.1
+# Porta: 3306
+# Usuário: root
+# Senha: 123456
+
